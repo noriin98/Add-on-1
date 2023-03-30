a0d966813bab0907a01caa6a0d022f9f579f2e5e
noriin98 <125356694+noriin98@users.noreply.github.com>
2023-03-30 11:13:04 +0200

    feil

---
+++ script.js
@@ -0,0 +1,5 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+losAngelesDateElement.innerHTML = moment().format("MMMM");
