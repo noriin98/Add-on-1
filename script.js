4d8b281dfd84e87c9fc4570472e6e5d929b215bc
noriin98 <125356694+noriin98@users.noreply.github.com>
2023-04-02 16:42:40 +0200

    endring

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-function updateTime() {
-let osloElement = document.querySelector ("#oslo");
-if ("osloElement") {
-let osloDateElement = document.querySelector(".dato");
-let osloTimeElement = document.querySelector(".tid");
-let osloTime = moment().tz("Europe/Oslo");
+function updateTime() {	
+let osloElement = document.querySelector("#oslo");	
+if ("osloElement") {	
+let osloDateElement = document.querySelector(".dato");	
+let osloTimeElement = document.querySelector(".tid");	
+let osloTime = moment().tz("Europe/Oslo");	
+osloDateElement.innerHTML = osloTime.format("MMMM Do YYYY");	
+osloTimeElement.innerHTML = osloTime.format("HH mm ss");	
+}	
+}	
+ function updateCity(event) {	
+let cityTimeZone = event.target.value;	
+if (cityTimeZone === "location") {	
+    cityTimeZone = moment.tz.guess();	
+}	
+let cityName = cityTimeZone.replace("_", " ").split("/")[1];	
+let cityTime = moment().tz(cityTimeZone);	
+let citiesElement = document.querySelector(".city");	
+citiesElement.innerHTML = `	
+<div class="city">	
+        <div class="row">	
+            <div class="col-6">	
+                <h2>${cityName}</h2>	
+                <br>	
+                <p class="dato">${cityTime.format("MMMM Do YYYY")}</p>	
+            </div>	
+            <div class="col-6">	
+                <h2 class="tid">${cityTime.format("HH mm ss")}</h2>	
+            </div>	
+        </div>	
+        </div>`;	
+ }	
 
-osloDateElement.innerHTML = osloTime.format("MMMM Do YYYY");
-osloTimeElement.innerHTML = osloTime.format("HH mm ss");
-}
-}
- function updateCity(event) {
-let cityTimeZone = event.target.value;
-if (cityTimeZone === "location") {
-    cityTimeZone = moment.tz.guess();
-}
-let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-let cityTime = moment().tz(cityTimeZone);
-let citiesElement = document.querySelector(".city");
-citiesElement.innerHTML = `
-<div class="city">
-        <div class="row">
-            <div class="col-6">
-                <h2>${cityName}</h2>
-                <br>
-                <p class="dato">${cityTime.format("MMMM Do YYYY")}</p>
-            </div>
-            <div class="col-6">
-                <h2 class="tid">${cityTime.format("HH mm ss")}</h2>
-            </div>
-        </div>
-        </div>`;
- }
 
-
-updateTime();
-setInterval(updateTime, 1000);
-
-let citiesSelectElement = document.querySelector("#cities");
-citiesSelectElement.addEventListener("change". updateCity);
+let citiesSelectElement = document.querySelector("#cities");	
+citiesSelectElement.addEventListener("change", updateCity);	
 setInterval(updateCity, 1000);
